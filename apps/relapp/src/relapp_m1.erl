10004743a82e8914c0bfb80c7768690ba13901c0
Luis Rascao <luis.rascao@gmail.com>
2018-08-31 11:03:10 +0100

    Add relapp_m1:method1/0

--- apps/relapp/src/relapp_m1.erl
+++ apps/relapp/src/relapp_m1.erl
@@ -1,9 +1,13 @@
 -module(relapp_m1).
 
--export([store_fun/0]).
+-export([store_fun/0,
+         method1/0]).
 
 store_fun() ->
     F = fun() ->
             io:format("hello friend, i'm a function\n")
         end,
     relapp_srv2:store_fun(F).
+
+method1() ->
+    ok.
